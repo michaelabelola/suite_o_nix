3a58f9c0affcbc7f5087380f9ec4ff68c39bb836
michaelabelola <michaelabelola@gmail.com>
2026-02-13 16:40:58 -0500

    fix

--- src/main/java/com/suiteonix/nix/kernel/configs/JobrunrConfig.java
+++ src/main/java/com/suiteonix/nix/kernel/configs/JobrunrConfig.java
@@ -1,18 +1,29 @@
 package com.suiteonix.nix.kernel.configs;
 
 import lombok.RequiredArgsConstructor;
+import org.jobrunr.storage.StorageProvider;
+import org.jobrunr.storage.StorageProviderUtils;
+import org.jobrunr.storage.sql.postgres.PostgresStorageProvider;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 @RequiredArgsConstructor
 public class JobrunrConfig {
 
+
+    @Bean
+    public StorageProvider storageProvider(DataSource dataSource) {
+        return new PostgresStorageProvider(dataSource, StorageProviderUtils.DatabaseOptions.CREATE);
+    }
+
 //    @Bean
 //    public StorageProvider storageProvider(DataSource dataSource, JobMapper jobMapper) {
-//        return new DefaultSqlStorageProvider(dataSource);
-//        SqlStorageProviderFactory sqlStorageProviderFactory = SqlStorageProviderFactory.using(dataSource);
-//        sqlStorageProviderFactory.withJobMapper(jobMapper);
-//        StorageProvider storageProvider = sqlStorageProviderFactory.create();
-//        return storageProvider;
+//        StorageProvider sqlStorageProviderFactory = SqlStorageProviderFactory.using(dataSource);
+//        sqlStorageProviderFactory.setJobMapper(jobMapper);
+//        sqlStorageProviderFactory.setUpStorageProvider(StorageProviderUtils.DatabaseOptions.CREATE);
+//        return sqlStorageProviderFactory;
 //    }
 }
