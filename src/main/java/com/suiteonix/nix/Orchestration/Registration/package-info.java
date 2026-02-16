c4451bd70baa7c686331ee16f969a5f0a1301544
michaelabelola <michaelabelola@gmail.com>
2026-02-15 21:28:58 -0500

    feature

---
+++ src/main/java/com/suiteonix/nix/Orchestration/Registration/package-info.java
@@ -0,0 +1,5 @@
+
+@ApplicationModule(id="user-registration", displayName = "User Registration")
+package com.suiteonix.nix.Orchestration.Registration;
+
+import org.springframework.modulith.ApplicationModule;
