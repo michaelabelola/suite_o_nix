59a7bbfed97d33d5b4975f18a8c19931c7b73a35
michaelabelola <michaelabelola@gmail.com>
2026-02-19 20:37:14 -0500

    feature

--- src/main/java/com/suiteonix/nix/kernel/JacksonConfig.java
+++ src/main/java/com/suiteonix/nix/kernel/JacksonConfig.java
@@ -1,8 +1,12 @@
 package com.suiteonix.nix.kernel;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import tools.jackson.databind.DeserializationFeature;
+import tools.jackson.databind.MapperFeature;
 import tools.jackson.databind.ObjectMapper;
+import tools.jackson.databind.SerializationFeature;
 import tools.jackson.databind.json.JsonMapper;
 
 import java.text.SimpleDateFormat;
@@ -16,6 +20,12 @@ public class JacksonConfig {
         return JsonMapper.builder()
                 .defaultDateFormat(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX"))
                 .defaultTimeZone(TimeZone.getTimeZone("Africa/Lagos"))
+                .changeDefaultPropertyInclusion(v -> v.withValueInclusion(JsonInclude.Include.NON_EMPTY))
+                .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+                .disable(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES)
+                .disable(SerializationFeature.FAIL_ON_EMPTY_BEANS)
+                .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES)
+                .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS)
                 .build();
     }
 }
